68432ce25aba46a6cbf731f18c035e32c9fd55d6
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-12-23 00:14:31 +0600

    pop up intregrate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='main-content-warp'>
+      <div className='container'>
+        <h1> ঘরে বসেই রান্নার মাধ্যমে আয় করুন ২০,০০০ থেকে ২৫,০০০ টাকা</h1>
+
+        <Popup trigger={<button className="button"> Open Modal </button>}
+          modal
         >
-          Learn React
-        </a>
-      </header>
+          {close => (
+            <div className="modal">
+              <button className="close" onClick={close}>
+                &times;
+              </button>
+              <div className="header"> Modal Title </div>
+              <div className="content">
+                <div class="login-warp">
+                  <input type="text" name="" id="" /><br />
+                  <input type="password" />
+                  <button>login</button>
+                </div>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </div>
+
+
+
     </div>
   );
-}
+};
 
 export default App;
